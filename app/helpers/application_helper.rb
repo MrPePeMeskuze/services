2f6ec08b40a58e3138b336adc192af51be44e700
Sergey <predatorqwerty2@gmail.com>
2015-05-30 10:53:56 +0300

    [+] Разбивка на паршелы

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(page_title)
+    standart_title = "Услуги строительства. Сопутствующие товары."
+    if page_title.empty?
+      standart_title
+    else
+      "#{page_title}"
+    end
+  end
 end
