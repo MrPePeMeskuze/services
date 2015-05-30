ecd4d05b1c036aebe59dcf40797572dc6f04f642
Sergey <predatorqwerty2@gmail.com>
2015-05-30 11:29:35 +0300

    [+] Именнованные пути и контроллер Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-
-  get 'home' , to: 'static_pages#home'
-
-  get '/' , to: 'static_pages#home'
-
-  get 'about' , to: 'static_pages#about'
-
-  get 'contacts' , to: 'static_pages#contacts'
-
+	root  'static_pages#home'
+	get 'home' , to: 'static_pages#home'
+	get 'about' , to: 'static_pages#about'
+	get 'contacts' , to: 'static_pages#contacts'
+	get 'signup', to: 'users#create'
 end
