fda55a228be05c3ea1f32a561e33a8ede6cafb88
Sergey <predatorqwerty2@gmail.com>
2015-06-17 21:52:45 +0300

    [+] Пользователь по умолч

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(login: "admin", email: "admin@admin.admin", is_admin: 1, name: "Administrator", password: "Beater69", password_confirmation: "Beater69")
