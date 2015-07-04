2afbcdb45944d1f782a296d0051be93383af0777
Sergey <predatorqwerty2@gmail.com>
2015-07-04 12:29:44 +0300

    no message

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,12 +15,17 @@ require 'bundler/capistrano'
 ## Чтобы не хранить database.yml в системе контроля версий, поместите
 ## dayabase.yml в shared-каталог проекта на сервере и раскомментируйте
 ## следующие строки.
+after "deploy:update_code", :copy_key_production
+ task :copy_key_production, roles => :app do
+   SECRET_KEY_BASE = "#{shared_path}/config/secrets.yml"
+   run "cp #{SECRET_KEY_BASE} #{release_path}/config/secrets.yml"
+ end
 
 after "deploy:update_code", :copy_database_config
  task :copy_database_config, roles => :app do
    db_config = "#{shared_path}/database.yml"
    run "cp #{db_config} #{release_path}/config/database.yml"
- end
+# end
 
 # В rails 3 по умолчанию включена функция assets pipelining,
 # которая позволяет значительно уменьшить размер статических
