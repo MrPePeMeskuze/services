74e1cd79521641dc880410ac16ef7d7fd81a4106
Sergey <predatorqwerty2@gmail.com>
2015-07-08 21:29:56 +0300

    no message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
-
+  set :precompile_cmd, "bundle exec rake RAILS_ENV=production RAILS_GROUPS=assets assets:precompile"
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
