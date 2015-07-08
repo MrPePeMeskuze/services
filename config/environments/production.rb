420c1937c43bbeabbdbe1f485bd34d4fdfd6ae84
Sergey <predatorqwerty2@gmail.com>
2015-07-08 20:59:08 +0300

    no message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
-
+  config.assets.version = '1.0'
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
