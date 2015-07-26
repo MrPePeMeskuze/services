a8fef04d6eb7dbdd6a68eab362d716389cbae064
Sergey <predatorqwerty2@gmail.com>
2015-07-26 11:13:32 +0300

    no message

--- app/models/services.rb
+++ app/models/services.rb
@@ -2,7 +2,6 @@ class Services < ActiveRecord::Base
 	require 'carrierwave/orm/activerecord'
 	validates :header, presence: true, uniqueness: true
 	validates :title, presence: true, uniqueness: true
-	validates :body, presence: true, uniqueness: true
 	validates :url, presence: true, uniqueness: true
 	def to_param
 		url
