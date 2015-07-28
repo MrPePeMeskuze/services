58f6b98cdea22edc71f48b94e3d4e7f1c7675b20
Sergey <predatorqwerty2@gmail.com>
2015-07-28 20:24:47 +0300

    Заголовок

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 
   def full_title(page_title)
-    standart_title = "Услуги строительства. Сопутствующие товары."
+    standart_title = 'Секреты Стройки" - cтроительные услуги.'
     if page_title.empty?
       standart_title
     else
-      "#{page_title}"
+      standart_title + &nbsp + "#{page_title}"
     end
   end
 	def nav_link(link_text, link_path)
